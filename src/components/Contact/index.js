a45099ca307c6363bb50ce4e83053103ae86571d
Mason Scott <masonscott141@gmail.com>
2025-03-05 00:34:02 -0500

    contact v1

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,22 @@ import './index.scss'
 
 const Contact = () => {
     return (
-        <div></div>
+        <div className='contact-container' >
+            <h1>Contact Me</h1>
+            <div className='contact-form' >
+                <form>
+                    <div className='row'>
+                        <input type='text' name='name' placeholder='Name' required />
+                        <input type='email' name='email' placeholder='Email' required />
+                    </div>
+                    <div className='column'>
+                        <input type='text' name='subject' placeholder='Subject' required />
+                        <textarea name='message' placeholder='Message' required ></textarea>
+                        <input type='submit' className='flat-button' value='Send' />
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- src/components/Landingpage/index.js
+++ src/components/Landingpage/index.js
@@ -13,18 +13,6 @@ const LandingPage = ({ projectRef, contactRef, aboutRef }) => {
                 <div className="text-zone">
                     <h1>Mason Scott</h1>
                     <h2>Data Science | Machine Learning | Software Engineering</h2>
-                    <div className='social-media-container'>
-                        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/mason-t-scott/'>
-                            <div>
-                                <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
-                            </div>
-                        </a>
-                        <a target="_blank" rel='noreferrer' href='https://github.com/04mscott'>
-                            <div>
-                                <FontAwesomeIcon icon={faGithubSquare} color='#4d4d4e' />
-                            </div>
-                        </a>
-                    </div>
                     <div className='button-container'>
                         <button className='flat-button' onClick={() => contactRef.current.scrollIntoView({behavior: 'smooth'})}>
                             Contact Me
@@ -32,6 +20,18 @@ const LandingPage = ({ projectRef, contactRef, aboutRef }) => {
                         <button className='flat-button' onClick={() => projectRef.current.scrollIntoView({behavior: 'smooth'})}>
                             Projects
                         </button>
+                    </div>
+                    <div className='social-media-container'>
+                        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/mason-t-scott/'>
+                            <div>
+                                <FontAwesomeIcon className='icon' icon={faLinkedin} color='#4d4d4e' />
+                            </div>
+                        </a>
+                        <a target="_blank" rel='noreferrer' href='https://github.com/04mscott'>
+                            <div>
+                                <FontAwesomeIcon className='icon' icon={faGithubSquare} color='#4d4d4e' />
+                            </div>
+                        </a>
                     </div>
                     <div className='scroll-arrow' >
                         <button rel='noreferrer' onClick={() => aboutRef.current.scrollIntoView({behavior: 'smooth'})} >
