31bbfa501c8b502975e180baa9a351d2aedec4a0
Mason Scott <masonscott141@gmail.com>
2026-09-21 04:19:14 -0400

    added shotline

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,6 +7,7 @@ import maskApp from '../../assets/images/incorrect_mask.png'
 import recAPI from '../../assets/images/api_flowchart.png'
 import impulse from '../../assets/images/impulse-demo.png'
 import engagecs from '../../assets/images/engagecs-graphs.png'
+import shotline from '../../assets/images/golf-stock-img.jpg'
 import Project from './project'
 import withScrollAnimation from '../Scroll'
 
@@ -20,20 +21,21 @@ const Projects = () => {
             <div className='project-grid' >
                 <div className='row'>
                     <div className='sub_row'>
+                        <AnimatedProject imgPath={shotline} link={'https://04mscott.github.io/Shotline-Golf-Analytics//'} title={'Shotline - Golf Analytics Platform (in development)'} langs={'Python, SQL'} tools={'Flask, PostgreSQL, Pandas, Numpy, Docker'} />
                         <AnimatedProject imgPath={engagecs} link={'https://04mscott.github.io/EngageCS/'} title={'EngageCS - Student Engagement Analytics Platform'} langs={'Python, SQL'} tools={'Dash, Flask, PostgreSQL, Celery, Docker, Canvas LMS API, Plotly'} />
-                        <AnimatedProject imgPath={impulse} link={'https://04mscott.github.io/Impulse-Music-Recs/'} title={'Impulse - Music Recommendation App'} langs={'JavaScript, Java, Python'} tools={'React, Spring Boot, FastAPI, Docker, Redis, Celery, MySQL'} />
                     </div>
                     <div className='sub_row'>
+                        <AnimatedProject imgPath={impulse} link={'https://04mscott.github.io/Impulse-Music-Recs/'} title={'Impulse - Music Recommendation App'} langs={'JavaScript, Java, Python'} tools={'React, Spring Boot, FastAPI, Docker, Redis, Celery, MySQL'} />
                         <AnimatedProject imgPath={recAPI} link={'https://04mscott.github.io/Recommendation-API/'} title={'Recommendation Engine API'} langs={'Python'} tools={'FastAPI, Pandas, Numpy, Sklearn, Docker, Celery, Redis'} />
-                        <AnimatedProject imgPath={airApp} link={'https://04mscott.github.io/Air-Quality-App//'} title={'Air Quality Prediction App - LSTM'} langs={'Python'} tools={'TensorFlow, Pandas, AWS, MySQL, StreamLit, OpenWeatherMap API'} />
                     </div>
                 </div>
                 <div className='row'>
                     <div className='sub_row'>
+                        <AnimatedProject imgPath={airApp} link={'https://04mscott.github.io/Air-Quality-App//'} title={'Air Quality Prediction App - LSTM'} langs={'Python'} tools={'TensorFlow, Pandas, AWS, MySQL, StreamLit, OpenWeatherMap API'} />
                         <AnimatedProject imgPath={maskApp} link={'https://04mscott.github.io/Face-Mask-Image-Classification/'} title={'Face Mask Image Classifier - CNN'} langs={'Python'} tools={'TensorFlow, Numpy, TaiPy'} />
-                        <AnimatedProject imgPath={sortingBm} link={'https://04mscott.github.io/Sorting-Functions-C-/'} title={'C Sorting Algorithms Benchmarks'} langs={'C, Python'} tools={'MatPlotLib, Seaborn, Numpy'} />
                     </div>
                     <div className='sub_row'>
+                        <AnimatedProject imgPath={sortingBm} link={'https://04mscott.github.io/Sorting-Functions-C-/'} title={'C Sorting Algorithms Benchmarks'} langs={'C, Python'} tools={'MatPlotLib, Seaborn, Numpy'} />
                         <AnimatedProject imgPath={reportPng} link={reportPdf} title={'Data Analysis Report'} langs={'Python'} tools={'Pandas, MatPlotLib, Seaborn, SciPy Stats, Numpy'} />
                     </div>
                 </div>
